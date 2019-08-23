fbe825295e837783d0aabd4ef6f4386ec470e55c
alexfiftysix <amiller5656@gmail.com>
2019-08-23 13:05:12 +1000

    ghost follows mouse until placed

--- Assets/scripts/BlockBuild.cs
+++ Assets/scripts/BlockBuild.cs
@@ -15,8 +15,33 @@ public class BlockBuild : MonoBehaviour {
 	private Vector2 dimensions; // dimensions of box being placed
 
 
+	private GameObject ghost;
+	private SpriteRenderer ghostSpriteRenderer;
+	private BoxCollider2D ghostCollider;
+
+	private Color canPlaceColor;
+	private Color cantPlaceColor;
+
 	// Use this for initialization
 	void Start () {
+		canPlaceColor = new Color(1, 0, 1, 0.5f); // magenta
+		cantPlaceColor = new Color(1, 0, 0, 0.5f); // red
+		GetNewGhost();
+	}
+
+	void GetNewGhost() {
+		// if (ghost != null) {
+		// 	Destroy(ghost);
+		// }
+		ghost = Instantiate(toPlaceDynamic);
+		ghostSpriteRenderer = ghost.GetComponent<SpriteRenderer>();
+		ghostSpriteRenderer.color = Color.clear; // otherwise it will flicker in spawn location beore heading to mouse
+		
+		ghostCollider = ghost.GetComponent<BoxCollider2D>();		
+		float width = ghostCollider.bounds.size.x;
+		float height = ghostCollider.bounds.size.y;
+		dimensions = new Vector2(width, height);
+		ghostCollider.enabled = false;
 	}
 	
 	
@@ -24,43 +49,28 @@ public class BlockBuild : MonoBehaviour {
 	void Update () {
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+		ghost.transform.position = mousePos2D;
+
+		RaycastHit2D boxCast = Physics2D.BoxCast(mousePos, dimensions, 0, Vector2.zero);
+		bool canPlace = boxCast.collider == null;
+
+		if (canPlace) {
+			ghostSpriteRenderer.color = canPlaceColor;
+		} else {
+			ghostSpriteRenderer.color = cantPlaceColor;
+		}
 
 		if (!currrentlyPlacing && Input.GetMouseButtonDown(0)) {
-			// make ghost
-			placing = Instantiate(toPlaceDynamic);
-			placing.GetComponent<SpriteRenderer>().color = new Color(1, 0, 1, 0.5f); // magenta, in theory
-			placing.GetComponent<BoxCollider2D>().isTrigger = true;
-
-			Collider2D placedCollider = placing.GetComponent<Collider2D>();
-			float width = placedCollider.bounds.size.x;
-			float height = placedCollider.bounds.size.y;
-			Debug.Log(width + ", " + height);
-			dimensions = new Vector2(width, height);
-			placing.GetComponent<BoxCollider2D>().enabled = false;
+			// store dimensions of ghost for collision calculation
 			currrentlyPlacing = true;
 		}
 
-		if (currrentlyPlacing && Input.GetMouseButton(0)) {
-			// drag around ghost
-			placing.transform.position = mousePos2D;
-		}
-
-		if (currrentlyPlacing && Input.GetMouseButtonUp(0)) {
-			// Check if colliding with anything
-			Vector3 centerLocation = new Vector3(mousePos.x, mousePos.y, 0);
-			RaycastHit2D boxCast = Physics2D.BoxCast(centerLocation, dimensions, 0, Vector2.zero);
-
-			if (boxCast.collider == null) {
-				// Not colliding, place object
-				placing.GetComponent<BoxCollider2D>().isTrigger = false;
-				placing.GetComponent<BoxCollider2D>().enabled = true;
-				placing.GetComponent<SpriteRenderer>().color = Color.black;
-				placing.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-			} else {
-				// Abort mission
-				Destroy(placing);
-			}
+		if (currrentlyPlacing && Input.GetMouseButtonUp(0) && canPlace) {
+			ghost.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			ghostSpriteRenderer.color = Color.black;
 			currrentlyPlacing = false;
+			ghostCollider.enabled = true;
+			GetNewGhost();
 		}
 	}
 }
