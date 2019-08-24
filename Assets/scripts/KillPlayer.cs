f1f95de86d02af2abfbc132574d191a8aabcf32c
Leo Baldock <leo@d-i89-172-111.student.eduroam.uq.edu.au>
2019-08-24 20:03:16 +1000

    Added player respawning

---
+++ Assets/scripts/KillPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+
+    // void OnTriggerEnter2D(Collider2D collider) {
+    //     Debug.Log(collider);
+    // }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log(collision);
+            GameMaster.KillPlayer(collision.gameObject.GetComponent<PlayerController>());
+        }
+    }
+
+}
