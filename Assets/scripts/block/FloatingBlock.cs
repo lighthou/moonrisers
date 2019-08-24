f306fecf6117060da14d2deabf69083518444368
Alexander Blyth <alexanderblyth5@gmail.com>
2019-08-24 21:24:21 +1000

    Add new block types

---
+++ Assets/scripts/block/FloatingBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingBlock : MonoBehaviour
+{
+    [SerializeField] private float frequency;
+    [SerializeField] private float magnitude;
+
+    private Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = position + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
+    }
+}
