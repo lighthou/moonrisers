8a4eb509bef6a690b7205942027bd687911629aa
Leo Baldock <leo@d-i89-172-111.student.eduroam.uq.edu.au>
2019-08-24 20:19:52 +1000

    Player respawning

--- Assets/scripts/KillPlayer.cs
+++ Assets/scripts/KillPlayer.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class KillPlayer : MonoBehaviour
 {
 
-    // void OnTriggerEnter2D(Collider2D collider) {
-    //     Debug.Log(collider);
-    // }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log(collision);
             GameMaster.KillPlayer(collision.gameObject.GetComponent<PlayerController>());
         }
     }
