5ad0a43834134ba8a9da819e8493539069107384
Alexander Blyth <alexanderblyth5@gmail.com>
2019-08-24 23:24:52 +1000

    Add floating block animation

---
+++ Assets/scripts/block/BlockBuilder.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class BlockBuilder : MonoBehaviour
+{
+    public BlockFactory blockFactory;
+    private bool currrentlyPlacing;
+
+    private GameObject ghost;
+    private BoxCollider2D ghostCollider;
+
+    private Color canPlaceColor;
+    private Color cantPlaceColor;
+
+    private Vector2 dimensions;
+
+    // Use this for initialization
+    void Start()
+    {
+        // TODO: Change this to use a scriptable object
+        canPlaceColor = new Color(1, 0, 1, 0.5f); // magenta
+        cantPlaceColor = new Color(1, 0, 0, 0.5f); // red
+
+        GetNewGhost();
+    }
+
+    void GetNewGhost()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        ghost = blockFactory.GetNextBlock(mousePos);
+
+        ghostCollider = ghost.GetComponent<BoxCollider2D>();
+        float width = ghostCollider.bounds.size.x;
+        float height = ghostCollider.bounds.size.y;
+        dimensions = new Vector2(width, height);
+        ghostCollider.enabled = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var canPlaceBlock = MoveBlock();
+
+        var colorToApply = canPlaceBlock ? canPlaceColor : cantPlaceColor;
+        ApplyColor(colorToApply);
+
+        if (!currrentlyPlacing && Input.GetMouseButtonDown(0))
+        {
+            currrentlyPlacing = true;
+        }
+
+        if (currrentlyPlacing && Input.GetMouseButtonUp(0) && canPlaceBlock)
+        {
+            PlaceBlock();
+            GetNewGhost();
+        }
+    }
+
+    /* Returns whether the block can be placed */
+    bool MoveBlock()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        ghost.transform.position = mousePos2D;
+        RaycastHit2D boxCast = Physics2D.BoxCast(mousePos, dimensions, 0, Vector2.zero);
+        return boxCast.collider == null;
+    }
+
+    void PlaceBlock()
+    {
+        var rigidbody = ghost.GetComponent<Rigidbody2D>();
+        if (rigidbody != null)
+        {
+            rigidbody.velocity = Vector2.zero;
+        }
+
+        var block = ghost.GetComponent<FloatingBlock>();
+        if (block != null)
+        {
+            block.PlaceBlock();
+        }
+
+        // Reset the color back to normal
+        ApplyColor(Color.white);
+        currrentlyPlacing = false;
+        ghostCollider.enabled = true;
+    }
+
+    void ApplyColor(Color color)
+    {
+        var sprites = ghost.GetComponentsInChildren<SpriteRenderer>();
+        foreach (var sprite in sprites)
+        {
+            sprite.color = color;
+        }
+    }
+}
--- Assets/scripts/block/FloatingBlock.cs
+++ Assets/scripts/block/FloatingBlock.cs
@@ -7,16 +7,40 @@ public class FloatingBlock : MonoBehaviour
     [SerializeField] private float frequency;
     [SerializeField] private float magnitude;
 
+    private float startTime;
+
     private Vector3 position;
+
+    [SerializeField] 
+    private bool isPlaced = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (isPlaced) {
+            position = transform.position;
+        }
+        startTime = Time.time;
+    }
+
+    public void PlaceBlock()
+    {
+        isPlaced = true;
         position = transform.position;
     }
 
     void Update()
     {
-        transform.position = position + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
+        var offset = Mathf.Sin(startTime - Time.time * frequency) * magnitude;
+        if (isPlaced)
+        {
+            // If the block has been placed, move it relative to where it was placed
+            transform.position = position + transform.up * offset;
+        }
+        else
+        {
+            // If the block is being placed, move it relative to where it currently is (the player's mouse)
+            transform.position = transform.position + transform.up * offset;
+        }
     }
 }
