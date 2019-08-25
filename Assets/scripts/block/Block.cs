963e6948ee361d4ceed75c07fa7461ec93fb4751
Alexander Blyth <alexanderblyth5@gmail.com>
2019-08-25 11:42:11 +1000

    Implement explosion particle effect & unstable block

--- Assets/scripts/block/Block.cs
+++ Assets/scripts/block/Block.cs
@@ -1,13 +1,43 @@
 using UnityEngine;
 
-public class Block : MonoBehaviour
+public class Block : MonoBehaviour, IBlock
 {
+    public int health { get; private set; }
+
+    public void Start()
+    {
+        health = 100;
+    }
+
+    public void PlaceBlock()
+    {
+        health = 100;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
     void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(1))
         {
             Debug.Log("Blam!");
-            Destroy(gameObject);
+            TakeDamage(20);
         }
     }
 }
---
+++ Assets/scripts/block/UnstableBlock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnstableBlock : MonoBehaviour, IBlock
+{
+    [SerializeField] private float ySpeed;
+    [SerializeField] private float xSpeed;
+    [SerializeField] private float magnitude;
+    private float startTime;
+    private Vector3 position;
+    private Block block;
+
+    public void Start()
+    {
+        // TODO: Remove this, for debugging purposes only 
+        startTime = Time.time;
+        position = transform.position;
+        block = GetComponent<Block>();
+    }
+
+    public void PlaceBlock()
+    {
+        startTime = Time.time;
+        position = transform.position;
+    }
+
+    public void Update()
+    {
+        var deltaTime = startTime - Time.time;
+        var xOffset = Mathf.Sin(deltaTime * xSpeed) * magnitude * deltaTime;
+        var yOffset = Mathf.Sin(deltaTime * ySpeed) * magnitude * deltaTime;
+        transform.position = position + transform.up * yOffset + transform.right * xOffset;
+        block.TakeDamage(1);
+    }
+}
