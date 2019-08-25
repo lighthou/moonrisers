c99a2f9b0b595528c9e5affb1ba98c889b909eea
Alexander Blyth <alexanderblyth5@gmail.com>
2019-08-25 14:27:52 +1000

    Add bombs to game

---
+++ Assets/scripts/AI/Bomb.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public ParticleSystem explosion;
+    public float radius = 5.0F;
+    public float power = 1.0F;
+    public float dropTime = 1;
+
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime >= dropTime)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+        foreach (Collider2D hit in colliders)
+        {
+            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                Debug.Log(rb);
+                var force = (rb.transform.position - explosionPos).normalized * power;
+                if (hit.gameObject.tag == "Player") {
+                    force = force / 10;
+                }
+                rb.AddForce(force, ForceMode2D.Impulse);
+            }
+        }
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    public static void AddExplosionForce(Rigidbody2D body, float explosionForce, Vector3 explosionPosition, float explosionRadius)
+    {
+        var dir = (body.transform.position - explosionPosition);
+        float wearoff = 1 - (dir.magnitude / explosionRadius);
+        body.AddForce(dir.normalized * explosionForce);
+    }
+}
---
+++ Assets/scripts/AI/BombDropper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombDropper : MonoBehaviour
+{
+    public float dropRate;
+    public float dropIncreaseRate;
+
+    public GameObject bomb;
+
+    private float lastDropped;
+
+    private float startTime;
+
+    private float nextDropTime;
+
+
+    void Start()
+    {
+        lastDropped = Time.time;
+        startTime = Time.time;
+        CalculateNextDropTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextDropTime <= Time.time)
+        {
+            DropBomb();
+            lastDropped = Time.time;
+            CalculateNextDropTime();
+        }
+    }
+
+    void DropBomb()
+    {
+        Debug.Log("Dropping next bomb!");
+        Vector2 position = new Vector2(Random.Range(-5f, 5f), Camera.main.transform.position.y + 4);
+        Instantiate(bomb, position, Quaternion.identity);
+    }
+
+    void CalculateNextDropTime()
+    {
+        var deltaTime = Time.time - startTime;
+
+        var modifier = Mathf.Clamp(dropRate - deltaTime * dropIncreaseRate, dropRate / 4, dropRate);
+        nextDropTime = lastDropped + modifier;
+    }
+}
